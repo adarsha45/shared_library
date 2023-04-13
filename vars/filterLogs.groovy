1cfe2d87f311cefcfafa6eb28e1a9ad87fb86c6e
oboe_ada <45120888+adarsha45@users.noreply.github.com>
2023-04-13 15:15:42 +0545

    Update filterLogs.groovy
    s

--- vars/filterLogs.groovy
+++ vars/filterLogs.groovy
@@ -5,6 +5,7 @@ import org.apache.commons.lang.StringUtils
 def call(String filter_string, int occurrence) {
     def logs = currentBuild.rawBuild.getLog(10000).join('\n')
     int count = StringUtils.countMatches(logs, filter_string);
+    sh """ echo `this is oboe` """
     if (occurrence == 50) {
         currentBuild.result='UNSTABLE'
     }
