24089ea509beb4be07f0953e8fe1c6ba643f471c
oboe_ada <45120888+adarsha45@users.noreply.github.com>
2023-04-18 15:22:32 +0545

    Update createDelta.groovy

--- vars/createDelta.groovy
+++ vars/createDelta.groovy
@@ -9,7 +9,7 @@ def call(Map args){
         throw new IllegalArgumentException("Both uiDeltaPath and cqDeltaPath cannot be empty. Please provide at least one of them.")
     }
   if (uiDeltaPath) {
-    sh "sudo sfdx plugins:install sfdx-git-delta"
+    sh "sudo sfdx plugins:install sfdx-git-delta | echo 'y'"
     def props = readJSON file: 'sfdx-project.json';
     SOURCE_API_NAME = props.sourceApiVersion;
     sh "rm -rf ui-delta"
