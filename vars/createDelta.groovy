85ef2c7a88a478a8b3549380566e030ec21780f3
oboe_ada <45120888+adarsha45@users.noreply.github.com>
2023-04-18 10:44:18 +0545

    Update createDelta.groovy

--- vars/createDelta.groovy
+++ vars/createDelta.groovy
@@ -3,11 +3,13 @@ def call(Map args){
   def uiDeltaPath = args.uiDeltaPath ?: ''
   def cqDeltaPath = args.cqDeltaPath ?: ''
   if (uiDeltaPath) {
+    sh "rm -rf ${uiDeltaPath}"
     sh "echo ${uiDeltaPath} feature:${featureBranchName}"
     sh "mkdir -p ${uiDeltaPath}"
     }
 
     if (cqDeltaPath) {
+      sh "rm -rf ${cqDeltaPath}"
       sh "echo ${cqDeltaPath} feature:${featureBranchName}"
       sh "mkdir -p ${cqDeltaPath}"
 
