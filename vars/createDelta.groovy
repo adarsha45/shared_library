749ba73f2c9c4bd36b80dade799401dde99a1bce
oboe_ada <45120888+adarsha45@users.noreply.github.com>
2023-04-18 10:29:40 +0545

    Update createDelta.groovy

--- vars/createDelta.groovy
+++ vars/createDelta.groovy
@@ -1,7 +1,7 @@
 def call(Map args){
   def featureBranchName = args.featureBranchName ?: 'default'
-  def uiDeltaPath = args.uiDeltaPath ?: ' '
-  def cqDeltaPath = args.cqDeltaPath ?: ' '
+  def uiDeltaPath = args.uiDeltaPath ?: ''
+  def cqDeltaPath = args.cqDeltaPath ?: ''
   if (uiDeltaPath) {
     sh "echo ${uiDeltaPath} feature:${featureBranchName}"
     }
