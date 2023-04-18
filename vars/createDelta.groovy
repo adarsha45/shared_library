a987750ab7becbf237046f3c88cb3b1f3b2c2960
oboe_ada <45120888+adarsha45@users.noreply.github.com>
2023-04-18 10:26:22 +0545

    Update createDelta.groovy

--- vars/createDelta.groovy
+++ vars/createDelta.groovy
@@ -1,3 +1,13 @@
-def call(){
-  println "this is adarsha"
+def call(Map args){
+  def featureBranchName = args.featureBranchName ?: 'default'
+  def uiDeltaPath = args.uiDeltaPath ?: 'ui-delta'
+  def cqDeltaPath = args.cqDeltaPath ?: 'cq-delta'
+  if (uiDeltaPath) {
+    sh "echo ${uiDeltaPath} feature:${featureBranchName}"
+    }
+
+    if (cqDeltaPath) {
+      sh "echo ${cqDeltaPath} feature:${featureBranchName}"
+    }
+
 }
