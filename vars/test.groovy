2cf69bbd6d62d1e784f66a449c344b4b276d6347
oboe_ada <45120888+adarsha45@users.noreply.github.com>
2023-04-21 04:57:00 +0545

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,8 +1,9 @@
 def call(){
-  pipeline {
-        agent any
-        parameters {
-            booleanParam(name: 'IS_VALIDATION', defaultValue: pipelineParams.IS_VALIDATION, description: 'Validates Packages in org by deploying source')
-            booleanParam(name: 'MANAGED_BETA', defaultValue: pipelineParams.MANAGED_BETA, description: 'Validates package while creating version')
-        }}
-}
+properties([
+        disableConcurrentBuilds(),
+        parameters([
+            booleanParam(name: 'IS_VALIDATION', defaultValue: 'true', description: 'Validates Packages in org by deploying source'),
+            booleanParam(name: 'MANAGED_BETA', defaultValue: 'true', description: 'Validates package while creating version')
+        ])
+    ])
+
