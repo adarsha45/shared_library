6ecc2bcc662b114a2be48cc19d605752e52e23be
oboe_ada <45120888+adarsha45@users.noreply.github.com>
2023-04-21 04:55:27 +0545

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,8 @@
 def call(){
-  def result = load('./file2.groovy').call()
+  pipeline {
+        agent any
+        parameters {
+            booleanParam(name: 'IS_VALIDATION', defaultValue: pipelineParams.IS_VALIDATION, description: 'Validates Packages in org by deploying source')
+            booleanParam(name: 'MANAGED_BETA', defaultValue: pipelineParams.MANAGED_BETA, description: 'Validates package while creating version')
+        }}
 }
