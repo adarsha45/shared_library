b15b1807c5476a7e649db64ccae4e2c6dfae2eef
oboe_ada <45120888+adarsha45@users.noreply.github.com>
2023-04-25 10:51:52 +0545

    Update createTest.groovy

--- vars/createTest.groovy
+++ vars/createTest.groovy
@@ -1,12 +1,11 @@
-//def SF_CONSUMER_KEY=env.SF_CONSUMER_KEY
+def call(){
+ //def SF_CONSUMER_KEY=env.SF_CONSUMER_KEY
        def SF_USERNAME="adarshashrestha957@cunning-hawk-qaejzs.com"
 //     def SERVER_KEY_CREDENTALS_ID=env.SERVER_KEY_CREDENTALS_ID
        SF_DEV_HUB_ALIAS="devHub"
        def SF_SCRATCH_ALIAS="testOrg"
      SF_DEV_INSTANCE_URL ="https://login.salesforce.com"
-    
-def call(){
  stage("Authorize a devhub"){
-        sh "sfdx auth:web:login -a ${SF_DEV_HUB_ALIAS} -instanceurl ${SF_DEV_INSTANCE_URL}"
+        sh "sfdx auth:web:login -a ${SF_DEV_HUB_ALIAS} -r ${SF_DEV_INSTANCE_URL}"
     }  
 }
