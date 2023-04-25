f18cfe4ca981b6467e47243bd67765fb5eef546a
oboe_ada <45120888+adarsha45@users.noreply.github.com>
2023-04-25 11:27:15 +0545

    Update createTest.groovy

--- vars/createTest.groovy
+++ vars/createTest.groovy
@@ -7,6 +7,6 @@ def call(){
      SF_DEV_INSTANCE_URL ="https://login.salesforce.com"
  stage("Authorize a devhub"){
 //         sh "sfdx auth:web:login -a ${SF_DEV_HUB_ALIAS} -r ${SF_DEV_INSTANCE_URL}"
-        sh "sf org login web --set-default-dev-hub --alias ${SF_DEV_HUB_ALIAS}"
+        sh "sf org login web --set-default-dev-hub --alias ${SF_DEV_HUB_ALIAS} -r ${SF_DEV_INSTANCE_URL}"
     }  
 }
