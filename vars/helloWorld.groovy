1ade95c8b7f9ca9d8c91a24be49c7dda4e6c1585
oboe_ada <45120888+adarsha45@users.noreply.github.com>
2023-04-25 10:37:09 +0545

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,6 +1,10 @@
 def call() {
-  println "hello world"
-  ada()
+     stage('checkout source') {
+        checkout scm
+    }
+     stage("Authorize a devhub"){
+        sh "sfdx auth:device:login -a ${SF_DEV_HUB_ALIAS} --instanceurl ${SF_DEV_INSTANCE_URL}"
+    }        
 }
 def ada(){
 println "this is adarsh"
