7fc41385694f2df6b522f9de8ee8f790850aa256
oboe_ada <45120888+adarsha45@users.noreply.github.com>
2023-04-25 10:14:29 +0545

    Update createBetatest.groovy

--- vars/createBetatest.groovy
+++ vars/createBetatest.groovy
@@ -1,12 +1,12 @@
 @Library('shared-library@main') _
 import groovy.json.JsonSlurperClassic
-node{
 //     def SF_CONSUMER_KEY=env.SF_CONSUMER_KEY
        def SF_USERNAME="adarshashrestha957@cunning-hawk-qaejzs.com"
 //     def SERVER_KEY_CREDENTALS_ID=env.SERVER_KEY_CREDENTALS_ID
        def SF_DEV_HUB_ALIAS="devHub"
        def SF_SCRATCH_ALIAS="testOrg"
     def SF_DEV_INSTANCE_URL ="https://login.salesforce.com"
+def call(){
     
     stage('checkout source') {
         checkout scm
