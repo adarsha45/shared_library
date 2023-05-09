ceb396295ead867b793a5d8be77ce26567c7f590
oboe_ada <45120888+adarsha45@users.noreply.github.com>
2023-05-09 11:48:28 +0545

    Update createScratchOrg.groovy

--- vars/createScratchOrg.groovy
+++ vars/createScratchOrg.groovy
@@ -1,20 +1,20 @@
 #!/usr/bin/env groovy
 def call(Map params) {
     def jname = env.JOB_NAME
+    def build_number = env.BUILD_NUMBER
+    def alias = jname + build_number
     def g = "${env.JOB_NAME}"
     def org = [:]
-    echo "${jname}"
-    echo "${g}"
-//     org.name = params.name
-//     org.projectScratchDefPath = params.projectScratchDefPath
-//     org.durationDays = params.durationDays
-//     echo "Create scratch org ${org.name}"
-//     echo "${jname}"
+    org.name = params.name
+    org.projectScratchDefPath = params.projectScratchDefPath
+    org.durationDays = params.durationDays
+    echo "Create scratch org ${org.name}"
+    echo "${alias}"
 
     // Username identifies the org in later stages
-   // def create = shWithResult "sfdx force:org:create --definitionfile ${org.projectScratchDefPath} --json --setdefaultusername --durationdays ${org.durationDays} --setalias ${jName} --wait 10"
-    //org.username = create.username
-    //org.orgId = create.orgId
+   def create = shWithResult "sfdx force:org:create --definitionfile ${org.projectScratchDefPath} --json --setdefaultusername --durationdays ${org.durationDays} --setalias ${jName} --wait 10"
+    org.username = create.username
+    org.orgId = create.orgId
 
     // Password and instance useful for manual debugging after the build (if org kept)
     shWithStatus "sfdx force:user:password:generate --targetusername ${org.username}"
