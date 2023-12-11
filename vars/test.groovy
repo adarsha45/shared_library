fe8522968aba301c8b667129821c1e167f40bb98
oboe_ada <45120888+adarsha45@users.noreply.github.com>
2023-12-11 09:23:11 +0545

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,20 @@
 def call(){
-  println("this si adashis")
+            checkout([
+                $class: 'GitSCM', 
+                branches: [[name: "13.1.0"]], 
+                extensions: [
+                    [
+                        $class: 'SubmoduleOption',
+                        disableSubmodules: false,
+                        parentCredentials: true,
+                        recursiveSubmodules: true,
+                        trackingSubmodules: true
+                    ]
+                    ,[
+                        $class: 'WipeWorkspace'
+                    ],
+                    [$class: 'ChangelogToBranch', options: [compareRemote: 'origin', compareTarget: "13.0.0"]]
+                ],
+                userRemoteConfigs: [[credentialsId : 'BitbucketJenkinsCredential', url: 'https://bitbucket.org/compquest/sqx.git']]
+            ])
 }
